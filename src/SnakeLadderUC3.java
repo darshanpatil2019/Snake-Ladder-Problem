ac7d78d826efc15c005bf8f257e1e93da0b81476
Darshan Patil <iampatildarsh>
2022-12-24 21:25:07 +0530

    Player checks a Option i.e NoPlay,Ladder or Snake

--- src/SnakeLadderUC3.java
+++ src/SnakeLadderUC3.java
@@ -1,40 +1,69 @@
 import java.util.Random;
 
-public class SnakeLadderUC3 extends SnakeLadderUC2 {
-    int initialPosition = 0;
-    int option;
-    int ladder = 1;
-    int snake = 2;
-    int noPlay = 0;
+public class SnakeLadderUC3 {
 
-    // Checking option Ladder,Snake or NoPlay
+    //Constants
+    private static final int IS_NO_PLAY = 0;
+    private static final int IS_LADDER = 1;
+    private static final int IS_SNAKE = 2;
 
-    public void Option() {
-        Random random = new Random();
-        option = random.nextInt(3);
+    //Declaring Variables for Players Position
+    int playerPosition = 0;
+    int newPosition = 0;
 
-        //Checking conditions(Option)
+    // Random Class Object for Generating Random Numbers
+    Random randomNo = new Random();
 
-        if (option == ladder) {
-            initialPosition = initialPosition + rollDie;
-            System.out.println("Ladder");
-        } else if (option == snake) {
-            initialPosition = initialPosition - rollDie;
-            System.out.println("Snake");
-        } else {
-            initialPosition = initialPosition;
-            System.out.println("No Play");
+    //Method to Show Player Current Position
+    private void showPosition() {
+        //Displaying Player Position
+        System.out.println("Player Starting Position : "+ playerPosition);
+    }
+
+    //Method to Roll the Die and Get No between 1 to 6
+    private int rollDie() {
+        int dieNo = randomNo.nextInt(6)+1;
+        return dieNo;
+    }
+
+    //Method to Check for Option
+    private void optionPlay(int dieNo) {
+
+        int optionNo = randomNo.nextInt(3)+1;
+        System.out.println("Option No : "+ optionNo);
+
+        switch (optionNo) {
+            case IS_NO_PLAY:
+                System.out.println("Player got NO PLAY");
+                newPosition = 0;
+                break;
+            case IS_LADDER:
+                System.out.println("Yahoo!!! Player got LADDER");
+                playerPosition += dieNo;
+                break;
+            case IS_SNAKE:
+                System.out.println("OOPS!!! Player got SNAKE");
+                playerPosition -= dieNo;
+                if (playerPosition < 0) {
+                    playerPosition = 0;
+                }
+                break;
         }
-        if (initialPosition < rollDie) {
-            initialPosition = 0;
-        }
-        System.out.println("Player is at Position: " + initialPosition);
-
     }
 
     public static void main(String[] args) {
+        //Creating Object for Player
         SnakeLadderUC3 obj = new SnakeLadderUC3();
-        obj.DieRoll();
-        obj.Option();
+
+        //Showing Player Position
+        obj.showPosition();
+
+        //Show Die Rolling No
+        int dieNo = obj.rollDie();
+        System.out.println("Die Number for Player : "+ dieNo);
+
+        //Player going for Options
+        obj.optionPlay(dieNo);
+        obj.showPosition();
     }
 }
